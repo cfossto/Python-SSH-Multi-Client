f031c6d78f7a3c677cf1325c9b5d70cd15db72c6
cfossto <70951673+cfossto@users.noreply.github.com>
2023-10-12 14:16:27 +0200

    Working client

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import multihost_ssh
+#from sys import argv
+
+# Prepared for command line work
+
+# Get the Linux command
+#argument = argv[1:]
+#command = ""
+
+#for word in argument:
+#    command = command + word + " "
 
 mhssh = multihost_ssh.multi_host_ssh()
 
-mhssh.connect_multiple_hosts("ls -l /")
+command = "ls -l /"
+mhssh.connect_multiple_hosts(command)
