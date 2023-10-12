e62bd6b19fa37ad943cb015340d57eb0d9d2be40
cfossto <70951673+cfossto@users.noreply.github.com>
2023-10-12 14:30:42 +0200

    Cleanup / refactor

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-import multihost_ssh
+from paramiko_multi_client import multihost_ssh
+
+'''
+    We are trying out three things here:
+        1. To get a string from argv to serve as a CLI-command
+        2. Connect to a server via Paramiko and custom host-key
+        3. Connect to a the same server with asynciossh
+
+        Things are commented out because of testing.
+'''
+
 #from sys import argv
 
 # Prepared for command line work
-
 # Get the Linux command
 #argument = argv[1:]
 #command = ""
-
 #for word in argument:
 #    command = command + word + " "
 
