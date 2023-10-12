acd17d36d84a4ce7f4151fa60a56cc39e9644f2c
cfossto <70951673+cfossto@users.noreply.github.com>
2023-10-12 21:58:00 +0200

    Comments.

--- paramiko_multi_client/multihost_ssh.py
+++ paramiko_multi_client/multihost_ssh.py
@@ -5,19 +5,28 @@ class MultiHostSSH:
     # Prepared for multihost-setup
 
     # Unique keyfiles per host
+    # Hosts can also be IPs
     key_files = {
+
+        # Host
         'micozdtest.lo': {
-            'key':"/Users/christopherfossto/.ssh/id_rsa",
-            "username":'test_user'}
+            # Host key
+            'key': "/Users/christopherfossto/.ssh/id_rsa",
+            # Username to connect with
+            "username": 'test_user'}
     }
 
     def connect_multiple_hosts(self,command):
         p = ParamikoClient()
         try:
+            # Loop through all keys (hosts) in the key_files.
+            # Then set the parameters for making a connection with the custom client.
             for host in self.key_files.keys():
                 print(f"Output from server - {host}:")
                 key = self.key_files.get(host).get('key')
                 username = self.key_files.get(host).get('username')
+
+                # Use the custom client
                 p.create_client(host,username,key,command)
         except:
             print("Multihost function failed.")
