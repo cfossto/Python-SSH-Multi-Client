2366e2e8814619d1e79467cdb06a2ceec8a2389c
Christopher Fossto <70951673+cfossto@users.noreply.github.com>
2023-10-20 14:32:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from paramiko_multi_client.multihost_ssh import MultiHostSSH
+from sys import argv
 
-#from sys import argv
+# Read and compile the command from CLI
+argument = argv[1:]
+command = ""
+for word in argument:
+    command = command + word + " "
 
-#argument = argv[1:]
-#command = ""
-#for word in argument:
-#    command = command + word + " "
-
+# Start the program
 mhssh = MultiHostSSH()
-
-command = "ls -l /"
 mhssh.connect_multiple_hosts(command)
